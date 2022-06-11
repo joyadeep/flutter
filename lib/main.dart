a50c86e2c7b854511cf905102b8ea4c76a94c267
joyadeep <joyadeep.limbu@gmail.com>
2022-06-11 08:36:46 +0545

    Day 2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,30 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Todo App'),
+          title: const Text(
+            'Todo App',
+            style: TextStyle(
+              fontSize: 32,
+            ),
+          ),
           centerTitle: true,
         ),
         body: const Center(
-          child: Text('This is body'),
+          child: Text(
+            'This is body',
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
+              color: Color.fromARGB(255, 171, 55, 203),
+            ),
+          ),
         ),
-        floatingActionButton:
-            FloatingActionButton(onPressed: () {}, child: Text('Click')),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Text(
+              '+',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w100),
+            )),
       ),
     ),
   );
